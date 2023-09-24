39b914f952108401b2aa1b8f8dbb1e099bacbb48
JBinkowski <jan.binkowski96@gmail.com>
2023-09-24 13:37:29 +0200

    Added system-independent solution for path handling

--- ChAMP_DisributedScript.R
+++ ChAMP_DisributedScript.R
@@ -1,68 +1,109 @@
 source("utils/utils.R")
+set.seed(101) 
 
-run_distributed_champ <- function(path_ss, path_idats, output, array_type = "EPIC", force = TRUE, norm_type = "BMIQ", cores = 1, chunk_size = 50){
-  path_idats = correct_idats_path(path_idats)
-  check_if_file_exists(path_idats)
-  check_if_file_exists(path_ss)
-  n_csv_files <- count_files(path_idats, "SampleSheet", ".csv")
-  
-  if (n_csv_files > 1){stop("Remove Sample Sheet from idat directory")}
-  
-  count_files(path_idats, "IDATs", ".idat")
-  check_if_file_exists(output)
-  
-  sample_sheet <- read.csv(path_ss, row.names = 1)
+run_distributed_champ <-
+  function(path_ss,
+           path_idats,
+           output,
+           method = "champ",
+           array_type = "EPIC",
+           force = TRUE,
+           norm_type = "BMIQ",
+           cores = 1,
+           chunk_size = 50) {
 
-  n_rows <- length(row.names(sample_sheet))
-  check_if_necessary(chunk_size, n_rows)
-  
-  randomized_samples <- shuffle(rownames(sample_sheet))
-
-  for (i in 1:ceiling(n_rows / chunk_size)){
-
+    check_if_file_exists(path_idats)
+    check_if_file_exists(path_ss)
+    n_csv_files <- count_files(path_idats, "SampleSheet", ".csv")
+    
+    if (n_csv_files > 1) {
+      stop("Please remove SampleSheet from idat directory!")
+    }
+    
+    count_files(path_idats, "IDATs", ".idat")
+    check_if_file_exists(output)
+    
+    sample_sheet <- read.csv(path_ss, row.names = 1)
+    
+    n_rows <- length(row.names(sample_sheet))
+    check_if_necessary(chunk_size, n_rows)
+    
+    randomized_samples <- shuffle(rownames(sample_sheet))
+    
+    for (i in 1:ceiling(n_rows / chunk_size)) {
+      
       # Crete QC path
       QC_path <- create_dir(output, "QC", i)
-
-      # Norm path
-      Norm_path <- create_dir(output, "Norm", i)
-
-      cat("Run", i, "/", ceiling(n_rows / chunk_size), "Each up to: ", chunk_size ,"samples.", "\n")
-
+      
+      # norm path
+      norm_path <- create_dir(output, "Norm", i)
+      
+      cat(
+        "Run",
+        i,
+        "/",
+        ceiling(n_rows / chunk_size),
+        "Each up to: ",
+        chunk_size ,
+        "samples.",
+        "\n"
+      )
+      
       idx_start <- chunk_size * (i - 1) + 1
       idx_end <- (chunk_size * i)
-      if (idx_end > n_rows){idx_end <- n_rows}
+      
+      if (idx_end > n_rows) {
+        idx_end <- n_rows
+      }
+      
       temp_samples <- randomized_samples[idx_start:idx_end]
-      temp_sample_sheet <- sample_sheet[temp_samples, ]
-
-      temp_sample_sheet["Sample_Name"] <- rownames(temp_sample_sheet)
-      write.csv(temp_sample_sheet, glue(path_idats, "/temp_sample_sheet.csv"), sep=",")
-
-      mynorm_temp <- run_champ(path_idats = path_idats, QC_path = QC_path, Norm_path = Norm_path, array_type = array_type, force = force, norm_type = norm_type, cores = cores)
+      temp_sample_sheet <- sample_sheet[temp_samples,]
+      
+      temp_sample_sheet["Sample_Name"] <-
+        rownames(temp_sample_sheet)
+      
+      write.csv(temp_sample_sheet,
+                file.path(path_idats, "temp_sample_sheet.csv"),
+                sep = ",")
+      
+      mynorm_temp <-
+        run_champ(
+          path_idats = path_idats,
+          QC_path = QC_path,
+          Norm_path = norm_path,
+          method = method,
+          array_type = array_type,
+          force = force,
+          norm_type = norm_type,
+          cores = cores
+        )
+      
       delete_temp_sample_sheet(path_idats)
-
-      path = glue(output, i, "_temp_chunk", ".csv")
-      write.csv(mynorm_temp, path, sep=",")
-      cat("Saved chunk no. ", i, "\n")
-
+      
+      path = file.path(output, glue(i, "_temp_chunk", ".parquet"))
+      
+      mynorm_temp$CpG <- rownames(mynorm_temp)
+      write.parquet(mynorm_temp, path)
+      message("Saved chunk no. ", i, "\n")
+    }
+    
+    message("Loading chunks ...", "\n")
+    mynorms <- load_chunks(output, "_temp_chunk.parquet")
+    
+    message("Looking for CpGs common across batches ...", "\n")
+    common_cpg <- overlap_cpgs(mynorms)
+    
+    message("Concating batches into myNorm ...", "\n")
+    myNorm <- concate_mynorms(mynorms, common_cpg)
+    
+    mynorm_path = glue(output, "myNorm.csv")
+    cat("Saving myNorm ...", "\n")
+    
+    write.csv(myNorm, mynorm_path)
+    
+    cat("Removing temporary files ...", "\n")
+    delete_temp_files(output, "temp_chunk.csv")
+    
+    message("DONE!")
+    
   }
-
-  cat("Loading chunks ...", "\n")
-  mynorms <- load_chunks(output, "_temp_chunk.csv")
-
-  cat("Looking for CpGs common across batches ...", "\n")
-  common_cpg <- overlap_cpgs(mynorms)
-  
-  cat("Concating batches into myNorm ...", "\n")
-  myNorm <- concate_mynorms(mynorms, common_cpg)
-  
-  mynorm_path = glue(output, "myNorm.csv")
-  cat("Saving myNorm ...", "\n")
-  
-  write.csv(myNorm, mynorm_path)
-
-  cat("Removing temporary files ...", "\n")
-  delete_temp_files(output, "temp_chunk.csv")
-  
-  cat("DONE!")
-  
-}
--- utils/utils.R
+++ utils/utils.R
@@ -1,147 +1,147 @@
 library(permute)
 library(ChAMP)
+library(arrow)
 library(glue)
 
-correct_idats_path <- function(path_idats){
-  if (endsWith(path_idats, "/")){
-    substr(path_idats, 1, nchar(path_idats) - 1)
-  }
-  else{
-    return(path_idats)
-  }
-}
-
-delete_temp_sample_sheet <- function(path_idats){
-  temp_file = glue(path_idats, "temp_sample_sheet.csv")
+delete_temp_sample_sheet <- function(path_idats) {
+  temp_file = file.path(path_idats, "temp_sample_sheet.csv")
   if (file.exists(temp_file)) {
     file.remove(temp_file)
   }
 }
 
 
-check_if_file_exists <- function(path){
+check_if_file_exists <- function(path) {
   if (!(file.exists(path))) {
-    cat("Dir", path, "does not exists", "\n")
+    message("Dir", path, "does not exists", "\n")
   }
 }
 
-count_files <- function(path, file_type, file_format){
+count_files <- function(path, file_type, file_format) {
   files = length(list.files(path, file_format))
-  cat("Founded", file_type, "no.:", files, "\n")
+  message("Found", file_type, "no.:", files, "\n")
   return(length(files))
 }
 
-check_if_necessary <- function(chunk_size, samples_number){
-  if(chunk_size > samples_number){
-    cat("Chunk size", chunk_size, ">", samples_number, "samples number", "\n")
-    stop("Stopped.")
+check_if_necessary <- function(chunk_size, samples_number) {
+  if (chunk_size > samples_number) {
+    message("Chunk size",
+            chunk_size,
+            ">",
+            samples_number,
+            "samples number",
+            "\n",
+            "Stopped.")
+    stop()
   }
 }
 
-create_dir <- function(path, name, extension){
-  path = glue(path, name, "_", extension)
+create_dir <- function(path, name, extension) {
+  path = file.path(path, glue(name, "_", extension))
   
   if ((file.exists(path))) {
-    cat("Dir", path, "already exists", "\n")
-    stop("Stopped.")
+    message("Dir", path, "already exists", "\n", "Stopped.")
+    stop()
   }
   
   dir.create(path)
   return(path)
 }
 
-overlap_cpgs <- function(list_of_mynorms_mynorms){
-  
+overlap_cpgs <- function(list_of_mynorms_mynorms) {
   cpgs_per_mynorm <- list()
   idx <- 1
   
-  for (mynorm in list_of_mynorms_mynorms){
+  for (mynorm in list_of_mynorms_mynorms) {
     cpgs_per_mynorm[[idx]] <- row.names(mynorm)
     idx <- idx + 1
   }
   common_cpgs <- Reduce(intersect, cpgs_per_mynorm)
-  cat("Found: ", length(common_cpgs), "common across chunks.", "\n")
+  message("Found: ", length(common_cpgs), "common across chunks.", "\n")
+  
   return(common_cpgs)
 }
 
-concate_mynorms <- function(list_of_mynorms, cpgs_common){
-  
+concate_mynorms <- function(list_of_mynorms, cpgs_common) {
   idx <- 1
-  for (mynorm in list_of_mynorms){
-    list_of_mynorms[[idx]] <- mynorm[cpgs_common, ]
+  for (mynorm in list_of_mynorms) {
+    list_of_mynorms[[idx]] <- mynorm[cpgs_common,]
     idx <- idx + 1
   }
   
   myNorm <- do.call("cbind", list_of_mynorms)
-  return(myNorm)
-}
-
-run_champ <- function(path_idats, QC_path, Norm_path, array_type, force, norm_type, cores){
-  
-  myLoad <- champ.load(directory = path_idats,
-                       method="champ",
-                       methValue="B",
-                       autoimpute=TRUE,
-                       filterDetP=TRUE,
-                       ProbeCutoff=0,
-                       SampleCutoff=0.1,
-                       detPcut=0.01,
-                       filterBeads=TRUE,
-                       beadCutoff=0.05,
-                       filterNoCG=TRUE,
-                       filterSNPs=TRUE,
-                       population=NULL,
-                       filterMultiHit=TRUE,
-                       filterXY=TRUE,
-                       force=force,
-                       arraytype=array_type)
-  
-  champ.QC(beta = myLoad$beta,
-           pheno=myLoad$pd$Sample_Group,
-           mdsPlot=TRUE,
-           densityPlot=TRUE,
-           dendrogram=TRUE,
-           PDFplot=TRUE,
-           Rplot=TRUE,
-           resultsDir=QC_path)
-  
-  myNorm <- champ.norm(beta=myLoad$beta,
-                       rgSet=myLoad$rgSet,
-                       mset=myLoad$mset,
-                       resultsDir=Norm_path,
-                       method=norm_type,
-                       plotBMIQ=TRUE,
-                       arraytype=array_type,
-                       cores = cores)
   
   return(myNorm)
 }
 
-load_chunks <- function(path, pattern){
-  
+run_champ <-
+  function(path_idats,
+           QC_path,
+           norm_path,
+           method,
+           array_type,
+           force,
+           norm_type,
+           cores) {
+    
+    myLoad <- champ.load(
+      directory = path_idats,
+      method = method,
+      methValue = "B",
+      force = force,
+      arraytype = array_type
+    )
+    
+    champ.QC(
+      beta = myLoad$beta,
+      pheno = myLoad$pd$Sample_Group,
+      mdsPlot = TRUE,
+      densityPlot = TRUE,
+      dendrogram = TRUE,
+      PDFplot = TRUE,
+      Rplot = TRUE,
+      resultsDir = QC_path
+    )
+    
+    myNorm <- champ.norm(
+      beta = myLoad$beta,
+      rgSet = myLoad$rgSet,
+      mset = myLoad$mset,
+      resultsDir = norm_path,
+      method = norm_type,
+      arraytype = array_type,
+      cores = cores
+    )
+    
+    return(myNorm)
+  }
+
+load_chunks <- function(path, pattern) {
   chunks <- list()
   files <- list.files(path, pattern)
-
+  
   idx <- 1
-  for (file in files){
-    chunk_path = glue(path, file)
-    cat("Loading: ", chunk_path, "\n")
+  for (file in files) {
+    chunk_path = file.path(path, file)
+    message("Loading: ", chunk_path, "\n")
     
-    chunk_df <- data.table::fread(chunk_path, data.table = FALSE)
-    chunk_df <- data.frame(chunk_df, row.names = 1)
-    chunks[[idx]] <- chunk_df 
+    chunk_df <- read.parquet(chunk_path)
+    rownames(chunk_df) <- chunk_df$CpG
+    chunk_df$CpG <- NULL
+    
+    chunks[[idx]] <- chunk_df
     idx <- idx + 1
   }
+  
   return(chunks)
 }
 
-delete_temp_files <- function(path, pattern){
+delete_temp_files <- function(path, pattern) {
   temp_files <- list.files(path, pattern)
   
-  for (file in temp_files){
+  for (file in temp_files) {
     chunk_path = glue(path, file)
-    cat("Deleting: ", chunk_path, "\n")
+    message("Deleting: ", chunk_path, "\n")
     unlink(chunk_path, recursive = FALSE, force = FALSE)
   }
 }
-
